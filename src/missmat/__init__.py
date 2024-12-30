c9c164142489d608c4b7979427457bc305db4562
Thijs van der Plas <vdplasthijs@gmail.com>
2024-12-30 14:12:24 +0000

    delete setuppy, init version automatic

--- src/missmat/__init__.py
+++ src/missmat/__init__.py
@@ -1,3 +1,4 @@
+from importlib.metadata import version
 from .vis_axes import (
     despine, naked, get_list_ticks, get_list_ticklabels, set_min_lims_to_0,
     equal_xy_lims, equal_lims_two_axs, equal_lims_n_axs, 
@@ -5,11 +6,10 @@ from .vis_axes import (
     add_panel_label
 )
 from .vis_text import (
-    set_font_size, two_digit_sci_not, readable_p, readable_p_exact,
+    set_fontsize, two_digit_sci_not, readable_p, readable_p_exact,
     readable_p_significance_statement
 )
-
-__version__ = '0.1.2'
+__version__ = version("missmate")
 __author__= 'Thijs van der Plas'
 __description__ = 'Simple functions to complement matplotlib'
 __all__ = ['vis_axes', 'vis_text']
